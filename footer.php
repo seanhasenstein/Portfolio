853e2fbaefda2c557e7a8d7ec07de860fdf9d10c
Sean Hasenstein <seanhasenstein@gmail.com>
2016-08-30 12:25:42 -0500

    Add GitHub profile link in footer

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
       <a href="mailto:seanhasenstein@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i></a>
       <a href="https://twitter.com/SeanHasenstein" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
       <a href="https://www.instagram.com/seanhasenstein" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+      <a href="https://github.com/seanhasenstein" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
       <a href="https://www.linkedin.com/in/sean-hasenstein-24088425" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
     </div>
     <p>Site powered by Sean.</p>
